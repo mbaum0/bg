f8b1539d99330dc189dce9b605a437e793bc362a
Michael Baumgarten <mbaum0@pm.me>
2024-01-10 18:07:28 +0100

    more sand!

--- src/sand.c
+++ src/sand.c
@@ -11,7 +11,10 @@ void click_cb(uint32_t eventType, SDL_Event* e, void* data) {
     if (eventType == SDL_MOUSEMOTION) {
         int x, y;
         SDL_GetMouseState(&x, &y);
-        Sand_create(sage, x, y);
+        Sprite* collision = VM_findSpriteAtCoordinate(sage->vm, x, y);
+        if (collision == NULL) {
+            Sand_create(sage, x, y);
+        }
     }
 }
 
@@ -20,17 +23,28 @@ void updateSand(ViewManager* vm, Sprite* sprite, void* data) {
     float newV = s->vel + (s->acc * .0116);
     int32_t newY = s->y + newV;
 
-    if (newY >= 600 - 1) {
-        s->y = 600 - 1;
+    if (newY >= 600 - 2) {
+        s->y = 600 - 2;
         return;
     }
 
-    Sprite* collision = VM_findSpriteCollision(vm, sprite);
-    if (collision == NULL) {
+    Sprite* collision = VM_findSpriteAtCoordinate(vm, s->x, newY);
+    if (collision == NULL){
         s->y = newY;
         s->vel = newV;
-        Sprite_setLocation(sprite, s->x, s->y);
+    } else {
+        while (collision != NULL) {
+            s->y = Sprite_getY(collision) - 2;
+            collision = VM_findSpriteAtCoordinate(vm, s->x, s->y);
+        }
+        
+        if (VM_findSpriteAtCoordinate(vm, s->x-2, s->y) == NULL) {
+            s->x -= 2;
+        } else if (VM_findSpriteAtCoordinate(vm, s->x+2, s->y) == NULL) {
+            s->x += 2;
+        }
     }
+    Sprite_setLocation(sprite, s->x, s->y);
 }
 
 Sand* Sand_create(Sage* sage, int32_t x, int32_t y) {
@@ -38,10 +52,10 @@ Sand* Sand_create(Sage* sage, int32_t x, int32_t y) {
     s->x = x;
     s->y = y;
     s->acc = 9.8;
-    s->vel = 0;
+    s->vel = 2;
 
     SDL_Texture* texture = Sage_loadTexture(sage, "assets/sand.png");
-    SDL_Rect src_rect = { 0, 0, 1, 1 };
+    SDL_Rect src_rect = { 0, 0, 2, 2 };
     Sprite* sprite = Sprite_create(texture, src_rect, x, y, 0, false);
     Sprite_registerUpdateFn(sprite, updateSand, s);
     Sage_registerSprite(sage, sprite);
