c5594f26e55377654ee3f1b36d3031a901953b11
Michael Baumgarten <mbaum0@pm.me>
2024-03-06 10:17:42 +0100

    roll for first refactoring

--- src/fsm/roll_for_first.c
+++ src/fsm/roll_for_first.c
@@ -6,70 +6,165 @@
 #include "fsm.h"
 #include "game.h"
 
-Uint32 timerRollDice(Uint32 interval, void* ctx) {
-    (void)interval;
-    (void)ctx;
-    FSMEvent e = {ROLL_DICE_EVENT, 0, NULL};
-    fsm_enqueue_event(e);
-    return 0;
-}
+// Uint32 timerRollDice(Uint32 interval, void* ctx) {
+//     (void)interval;
+//     (void)ctx;
+//     FSMEvent e = {ROLL_DICE_EVENT, 0, NULL};
+//     fsm_enqueue_event(e);
+//     return 0;
+// }
 
-Uint32 timerGetFirstPlayer(Uint32 interval, void* ctx) {
-    (void)interval;
-    GameBoard* gb = (GameBoard*)ctx;
+// Uint32 timerGetFirstPlayer(Uint32 interval, void* ctx) {
+//     (void)interval;
+//     GameBoard* gb = (GameBoard*)ctx;
 
-    Color firstPlayer = NONE; // default to NONE, which is a tie
+//     Color firstPlayer = NONE; // default to NONE, which is a tie
+//     if (gb->die1.value > gb->die2.value) {
+//         firstPlayer = DARK;
+//     } else if (gb->die2.value > gb->die1.value) {
+//         firstPlayer = LIGHT;
+//     }
+
+//     FSMEvent e = {ROLLED_FOR_FIRST_EVENT, firstPlayer, NULL};
+//     fsm_enqueue_event(e);
+//     return 0;
+// }
+
+// Uint32 timerTransitionToPlayerState(Uint32 interval, void* ctx) {
+//     (void)interval;
+//     (void)ctx;
+//     FSMEvent e = {GOT_FIRST_PLAYER_EVENT, 0, NULL};
+//     fsm_enqueue_event(e);
+//     return 0;
+// }
+
+Color getFirstPlayer(GameBoard* gb) {
     if (gb->die1.value > gb->die2.value) {
-        firstPlayer = DARK;
+        return DARK;
     } else if (gb->die2.value > gb->die1.value) {
-        firstPlayer = LIGHT;
+        return LIGHT;
     }
-
-    FSMEvent e = {ROLLED_FOR_FIRST_EVENT, firstPlayer, NULL};
-    fsm_enqueue_event(e);
-    return 0;
+    return NONE;
 }
 
-Uint32 timerTransitionToPlayerState(Uint32 interval, void* ctx) {
-    (void)interval;
-    (void)ctx;
-    FSMEvent e = {GOT_FIRST_PLAYER_EVENT, 0, NULL};
-    fsm_enqueue_event(e);
-    return 0;
+bool handleEnteredRollForFirstEvent(GameBoard* gb) {
+    rollDice(gb);
+    FSMEvent e = {ROLLED_FOR_FIRST_EVENT, 0, NULL};
+    fsm_enqueue_event_delay(500, e);
+    return false;
+}
+
+bool handleRollForFirstEvent(GameBoard* gb) {
+    Color fp = getFirstPlayer(gb);
+    if (fp == NONE) {
+        FSMEvent e = {TIED_ROLL_EVENT, 0, NULL};
+        fsm_enqueue_event_delay(500, e);
+    } else {
+        FSMEvent e = {GOT_FIRST_PLAYER_EVENT, 0, NULL};
+        fsm_enqueue_event_delay(500, e);
+    }
+    return false;
+}
+
+bool handleTiedRollEvent(GameBoard* gb) {
+    rollDice(gb);
+    FSMEvent e = {ROLLED_FOR_FIRST_EVENT, 0, NULL};
+    fsm_enqueue_event_delay(500, e);
+    return false;
+}
+
+bool handleGotFirstPlayerEvent(GameBoard* gb) {
+    Color fp = getFirstPlayer(gb);
+    if (fp == LIGHT) {
+        gb->activePlayer = LIGHT;
+        gb->die1.side = 1;
+        gb->die2.side = 1;
+    } else {
+        gb->activePlayer = DARK;
+        gb->die1.side = 0;
+        gb->die2.side = 0;
+    }
+    FSMEvent e = {FINISH_ROLL_FOR_FIRST_EVENT, 0, NULL};
+    fsm_enqueue_event_delay(500, e);
+    return false;
+}
+
+bool handleFinishRollForFirstEvent(GameBoard* gb){
+    (void)gb;
+    fsm_transition(PLAYER_MOVE_STATE);
+    return true;
 }
 
 void roll_for_first_state(FiniteStateMachine* fsm) {
     GameBoard* gb = &fsm->gb;
     (void)gb;
     FSMEvent event;
-    while (fsm_dequeue_event(&event)) {
-        if (event.etype == ROLL_DICE_EVENT) {
-            rollDice(gb);
-            SDL_AddTimer(500, timerGetFirstPlayer, gb);
-        }
+    bool quit = false;
+    while (fsm_dequeue_event(&event) && !quit) {
 
-        if (event.etype == ROLLED_FOR_FIRST_EVENT) {
-            if (event.code == LIGHT) {
-                gb->activePlayer = LIGHT;
-                gb->die1.side = 1;
-                gb->die2.side = 1;
-                SDL_AddTimer(400, timerTransitionToPlayerState, NULL);
-            } else if (event.code == DARK) {
-                gb->activePlayer = DARK;
-                gb->die1.side = 0;
-                gb->die2.side = 0;
-                SDL_AddTimer(400, timerTransitionToPlayerState, NULL);
-            } else {
-                // do another roll
-                SDL_AddTimer(500, timerRollDice, gb);
-                continue;
-            }
-        }
-
-        if (event.etype == GOT_FIRST_PLAYER_EVENT) {
-            fsm_transition(PLAYER_MOVE_STATE);
+        switch (event.etype) {
+        case ENTERED_ROLL_FOR_FIRST_STATE_EVENT:
+            quit = handleEnteredRollForFirstEvent(gb);
+            break;
+        case ROLLED_FOR_FIRST_EVENT:
+            quit = handleRollForFirstEvent(gb);
+            break;
+        case TIED_ROLL_EVENT:
+            quit = handleTiedRollEvent(gb);
+            break;
+        case GOT_FIRST_PLAYER_EVENT:
+            quit = handleGotFirstPlayerEvent(gb);
+            break;
+        case FINISH_ROLL_FOR_FIRST_EVENT:
+            quit = handleFinishRollForFirstEvent(gb);
+            break;
+        default:
             break;
         }
+
+        // if (event.etype == ROLL_DICE_EVENT) {
+        //     rollDice(gb);
+        //     FSMEvent e = {GET_FIRST_PLAYER_EVENT, 0, NULL};
+        //     fsm_enqueue_event_delay(500, e);
+        // }
+
+        // if (event.etype == GET_FIRST_PLAYER_EVENT) {
+        //     Color firstPlayer = NONE; // default to NONE, which is a tie
+        //     if (gb->die1.value > gb->die2.value) {
+        //         firstPlayer = DARK;
+        //     } else if (gb->die2.value > gb->die1.value) {
+        //         firstPlayer = LIGHT;
+        //     }
+
+        //     FSMEvent e = {ROLLED_FOR_FIRST_EVENT, firstPlayer, NULL};
+        //     fsm_enqueue_event(e);
+        // }
+
+        // if (event.etype == ROLLED_FOR_FIRST_EVENT) {
+        //     if (event.code == LIGHT) {
+        //         gb->activePlayer = LIGHT;
+        //         gb->die1.side = 1;
+        //         gb->die2.side = 1;
+        //         FSMEvent e = {GOT_FIRST_PLAYER_EVENT, 0, NULL};
+        //         fsm_enqueue_event_delay(400, e);
+        //     } else if (event.code == DARK) {
+        //         gb->activePlayer = DARK;
+        //         gb->die1.side = 0;
+        //         gb->die2.side = 0;
+        //         FSMEvent e = {GOT_FIRST_PLAYER_EVENT, 0, NULL};
+        //         fsm_enqueue_event_delay(400, e);
+        //     } else {
+        //         // do another roll
+        //         FSMEvent e = {ROLL_DICE_EVENT, 0, NULL};
+        //         fsm_enqueue_event_delay(500, e);
+        //         continue;
+        //     }
+        // }
+
+        // if (event.etype == GOT_FIRST_PLAYER_EVENT) {
+        //     fsm_transition(PLAYER_MOVE_STATE);
+        //     break;
+        // }
     }
 }
 void roll_for_first_init_state(FiniteStateMachine* fsm) {
@@ -78,5 +173,6 @@ void roll_for_first_init_state(FiniteStateMachine* fsm) {
     initBoardForRollForFirst(gb);
 
     // roll the dice
-    SDL_AddTimer(500, timerRollDice, gb);
+    FSMEvent e = {ENTERED_ROLL_FOR_FIRST_STATE_EVENT, 0, NULL};
+    fsm_enqueue_event_delay(500, e);
 }
